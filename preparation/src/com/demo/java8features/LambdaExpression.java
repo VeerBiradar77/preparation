f7a0d3863fe4ccfb400558177f6e2ce961e9affa
Veer Biradar <veerbiradar7@gmail.com>
2025-07-01 14:57:58 +0530

    .....

--- preparation/src/com/demo/java8features/LambdaExpression.java
+++ preparation/src/com/demo/java8features/LambdaExpression.java
@@ -9,11 +9,10 @@ public class LambdaExpression {
 	// they provide concise way to represent anonymous functions
 	// it is mostly used to pass behavior as a parameters(ex, sorting, filtering,
 	// iterating)
-	
-	//Key Point is - replaces the biolerplate anonymous classes.
 
-	public static void main(String[] args) {
+	// Key Point is - replaces the biolerplate anonymous classes.
 
+	public static void run() {
 		List<String> list = Arrays.asList("A", "B", "C");
 
 		// without lambda
@@ -23,6 +22,15 @@ public class LambdaExpression {
 
 		// with lambda
 		list.forEach(s -> System.out.println(s));
+		
+		//sorting using lambda
+		List<String>names=Arrays.asList("veer","raj","sandhya","shivam","aavni");
+		names.sort((a, b) -> a.compareTo(b));
+		System.out.println("Sorted names:" + names);
+		
+		//loop using lambda with foreach
+		
+		names.forEach(name -> System.out.println("Hello:"+ name));
 
 	}
 
---
+++ preparation/src/com/demo/java8features/MainDemo.java
@@ -0,0 +1,11 @@
+package com.demo.java8features;
+
+public class MainDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LambdaExpression.run();
+
+	}
+
+}
