e92f3a0aee40c509a930d22d576baa8e46bfea84
Veer Biradar <veerbiradar7@gmail.com>
2025-07-03 16:56:08 +0530

    Added comments

--- preparation/src/com/demo/designpatterns/factory/FactoryDemo.java
+++ preparation/src/com/demo/designpatterns/factory/FactoryDemo.java
@@ -3,12 +3,19 @@ package com.demo.designpatterns.factory;
 //Cliet code
 public class FactoryDemo {
 
+	// when and why to use factory method pattern
+	// use case => when we have superclass but let subclasses which class to
+	// instantiate
+	// CLient code should not depend on the concrete classes
+
+	// why => Loose coupling ->client code uses interface.
+	// open/closed principles Add new product without changing client code
 	public static void main(String[] args) {
 
 		Creater creator1 = new ConcreteCreatorA();
 		Product product1 = creator1.factoryMethod();
 		product1.create();
-		
+
 		Creater creator2 = new ConcreteCreatorB();
 		Product product2 = creator2.factoryMethod();
 		product2.create();
